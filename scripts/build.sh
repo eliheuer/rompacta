e4d9b2d5365286038ecf94e700eac737bcaa17d2
eli <elih@protonmail.com>
2020-03-29 12:58:14 -0400

    Add basic build script

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "[INFO] Building Rompacta with Fontmake..."
+fontmake -u sources/rompacta.ufo
+echo "[INFO] Done building Rompacta with Fontmake..."
+
+echo "[INFO] Moving font output..."
+cp master_ttf/rompacta.ttf fonts/static/Rompacta.ttf
+echo "[INFO] Done moving font output..."
+
+echo "[INFO] Removing font build directories..."
+rm -rf master_otf master_ttf
+echo "[INFO] Done removing font build directories..."
