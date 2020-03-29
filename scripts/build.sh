5157e3c5402d7dea7f88d1f4d3a0d41fbb99d446
eli <elih@protonmail.com>
2020-03-29 13:36:53 -0400

    Update build script with dsig fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,3 +12,7 @@ echo "[INFO] Done moving font output..."
 echo "[INFO] Removing font build directories..."
 rm -rf master_otf master_ttf
 echo "[INFO] Done removing font build directories..."
+
+echo "[INFO] Fixing dsig table with gftools..."
+gftools fix-dsig fonts/static/Rompacta.ttf --autofix
+echo "[INFO] Done fixing dsig table with gftools..."
