b34e659a536b1a9e310bc217c57b1d3769f5eab7
eli <elih@protonmail.com>
2020-03-29 11:15:41 -0400

    Add Python virtual environment setup script

---
+++ scripts/python-virtual-environment-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "[INFO] Building Python3 virtual environment..."
+python3 -m venv rompacta-virtual-env
+echo "[INFO] Done building Python3 virtual environment..."
+
+#echo "[INFO] Activating Python3 virtual environment..."
+#./rompacta-virtual-env/bin/activate
+#echo "[INFO] Done activating Python3 virtual environment..."
+
+#pip install -r requierments.txt
